b9ebfb5b05ce3b1dbe5b478b3ad9d2988686368b
Jaemin <once29@naver.com>
2022-06-16 13:12:05 +0900

    Add package to HelloController

--- src/main/kotlin/com/funin/kubekopringdemo/controller/HelloController.kt
+++ src/main/kotlin/com/funin/kubekopringdemo/controller/HelloController.kt
@@ -1,12 +1,12 @@
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
+package com.funin.kubekopringdemo.controller
 
-@Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
 
-    @ResponseBody
-    @GetMapping("/")
+    @RequestMapping("/hello")
     fun hello(): String {
         return "Hello-world!"
     }
